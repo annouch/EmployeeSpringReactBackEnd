7520297e7af31ae12132f7cb561e7f6b5cabea5d
ANNOUCH <ANNOUCH@DESKTOP-5R512IQ>
2021-01-22 21:47:19 +0100

    initial commit of the project.

---
+++ springboot-backend/src/main/java/com/yass/springboot/controller/EmployeeController.java
@@ -0,0 +1,26 @@
+package com.yass.springboot.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.yass.springboot.model.Employee;
+import com.yass.springboot.repository.EmployeeRepository;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api/")
+public class EmployeeController {
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	@GetMapping("/employees")
+	public List<Employee> getAllEmployee(){
+		return employeeRepository.findAll();
+	}
+}
