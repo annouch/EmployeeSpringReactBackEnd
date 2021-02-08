6bd7f5df4bc926979e3eb100ce2c26393be0a3ef
ANNOUCH <ANNOUCH@192.168.1.14>
2021-02-08 15:37:53 +0100

    Add Employee--1

--- springboot-backend/src/main/java/com/yass/springboot/controller/EmployeeController.java
+++ springboot-backend/src/main/java/com/yass/springboot/controller/EmployeeController.java
@@ -3,11 +3,17 @@ package com.yass.springboot.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.yass.springboot.exception.ResourceNotFoundException;
 import com.yass.springboot.model.Employee;
 import com.yass.springboot.repository.EmployeeRepository;
 
@@ -24,4 +30,22 @@ public class EmployeeController {
 		return employeeRepository.findAll();
 		//hello
 	}
+	
+	//create employee 
+	
+	@PostMapping("/employees")
+	public Employee saveEmoloyee(@RequestBody Employee employee) {
+		
+		return employeeRepository.save(employee) ;
+	}
+	
+	@GetMapping("/employees/{id}")
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+		
+		Employee employee = employeeRepository.findById(id).orElseThrow(() 
+								-> new ResourceNotFoundException("Employee not Found with the ID :" +id));
+		return ResponseEntity.ok(employee);
+		
+	}
+	
 }
